1eebf2a032f0ed769046228643dc45d9328c28f9
Vitor <vitor_fernandes_pie@yahoo.com.br>
2025-02-01 10:42:47 -0300

    feat: contact and footer styles

--- events.js
+++ events.js
@@ -3,6 +3,15 @@ window.addEventListener("scroll", function () {
     if (window.scrollY > 20) {
         header.classList.add("scrolled");
     } else {
-        header.classList.remove("scrolled")
+        header.classList.remove("scrolled");
+    }
+    let whatsappButton = this.document.getElementById("whatsapp-button");
+    if(this.window.scrollY > 400 && this.window.scrollY <2000) {
+        whatsappButton.style.opacity = "1";
+        whatsappButton.style.visibility = "visible";
+    } else {
+        whatsappButton.style.opacity = "0";
+        whatsappButton.style.visibility = "hidden";
     }
 })
+
