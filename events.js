432b64618b1bcd0292515ed851db052d1e17abb0
Vitor <vitor_fernandes_pie@yahoo.com.br>
2025-02-03 10:56:08 -0300

    feat: hamburger menu for mobile devices

--- events.js
+++ events.js
@@ -1,3 +1,31 @@
+// Hamburger menu
+
+const hamburger = document.getElementById("hamburger");
+const externalLinks = document.getElementById("external-links")
+const menu = document.getElementById("menu");
+const header = document.querySelector("header");
+const identification = document.getElementById("identification")
+const line = document.querySelector("hr")
+
+hamburger.addEventListener("click", () => {
+    header.classList.toggle("active");
+    menu.classList.toggle("active");
+    externalLinks.classList.toggle("active");
+    identification.classList.toggle("active");
+    hamburger.classList.toggle("active");
+    line.classList.toggle("active");
+
+    const icon = hamburger.querySelector("i");
+    if (menu.classList.contains("active")) {
+        icon.classList.replace("fa-bars", "fa-times");
+    } else {
+        icon.classList.replace("fa-times", "fa-bars");
+    }
+});
+
+
+// Scroll functions
+
 window.addEventListener("scroll", function () {
     let header = this.document.querySelector("header");
     if (window.scrollY > 20) {
