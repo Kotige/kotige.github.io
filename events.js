8b33c200f0d02d85866abc8000cb239cd15fa5bd
Vitor <vitor_fernandes_pie@yahoo.com.br>
2025-02-03 16:30:59 -0300

    fix: changes in responsivity

--- events.js
+++ events.js
@@ -1,11 +1,12 @@
 // Hamburger menu
 
 const hamburger = document.getElementById("hamburger");
-const externalLinks = document.getElementById("external-links")
+const externalLinks = document.getElementById("external-links");
 const menu = document.getElementById("menu");
 const header = document.querySelector("header");
-const identification = document.getElementById("identification")
-const line = document.querySelector("hr")
+const identification = document.getElementById("identification");
+const line = document.querySelector("hr");
+const menuLinks = document.querySelectorAll(".nav-link");
 
 hamburger.addEventListener("click", () => {
     header.classList.toggle("active");
@@ -23,15 +24,32 @@ hamburger.addEventListener("click", () => {
     }
 });
 
+menuLinks.forEach(link => {
+    link.addEventListener("click", () => {
+        header.classList.remove("active");
+        menu.classList.remove("active");
+        externalLinks.classList.remove("active");
+        identification.classList.remove("active");
+        hamburger.classList.remove("active");
+        line.classList.remove("active");
+
+        const icon = hamburger.querySelector("i");
+        icon.classList.replace("fa-times", "fa-bars");
+    });
+});
+
 
 // Scroll functions
 
 window.addEventListener("scroll", function () {
     let header = this.document.querySelector("header");
+    let hrLine = this.document.querySelector("hr")
     if (window.scrollY > 20) {
         header.classList.add("scrolled");
+        hrLine.classList.add("scrolled-hr")
     } else {
         header.classList.remove("scrolled");
+        hrLine.classList.remove("scrolled-hr");
     }
     let whatsappButton = this.document.getElementById("whatsapp-button");
     if(this.window.scrollY > 400 && this.window.scrollY <2000) {
